9bf3733dfbc60da6a8f5d2a422e94e79af34cdd7
Conan Kunhwan Ahn <kunhwon@gmail.com>
2023-03-05 10:46:47 +0900

    운영과 개발 설정 분리

--- next.config.js
+++ next.config.js
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
-const config = {
+const defaultConfig = {
   reactStrictMode: true,
   swcMinify: true,
+};
+
+/** @type {import('next').NextConfig} */
+const devConfig = {
+  ...defaultConfig,
+};
+
+/** @type {import('next').NextConfig} */
+const prodConfig = {
+  ...defaultConfig,
   compiler: {
     removeConsole: {
       exclude: ['error'],
     },
   },
 };
+
+/** @type {import('next').NextConfig} */
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
 
 module.exports = config;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <>
-      <div>Hello Next.js</div>
-    </>
+    <div>Hello Next.js</div>
   );
 }
