fc3698fd30bb5c1af8cefba7a2159524a6810d80
Conan Kunhwan Ahn <kunhwon@gmail.com>
2023-03-05 11:13:22 +0900

    Todo 기본 UI 추가

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from './TodoItem.module.scss';
+
+interface TodoItemProps {
+  id: number;
+  text: string;
+}
+export default function TodoItem({ id, text }: TodoItemProps) {
+  const removeTodo = (targetId: number) => {
+    console.log(`remove ${{ targetId }}`);
+  };
+
+  return (
+    <li className={styles['todo-item']}>
+      <Link href={`/${id}`}>
+        {text}
+      </Link>
+      <button
+        type="button"
+        onClick={() => { removeTodo(id); }}
+      >
+        삭제
+      </button>
+    </li>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import Head from 'next/head';
+
+import TodoForm from '@/components/TodoForm';
+import TodoItem from '@/components/TodoItem';
+
+interface TypeTodo {
+  id: number;
+  text: string;
+}
 
 export default function Home() {
+  const todoList: TypeTodo[] = [
+    {
+      id: 1,
+      text: 'TODO 1',
+    },
+    {
+      id: 2,
+      text: 'TODO 2',
+    },
+  ];
+
   return (
-    <div>Hello Next.js</div>
+    <>
+      <Head>
+        <title>TODO List</title>
+      </Head>
+      <div>{`TODO #${todoList.length}`}</div>
+      <TodoForm />
+      <ul>
+        {todoList.map((todoItem) => (
+          <TodoItem
+            key={todoItem.id}
+            id={todoItem.id}
+            text={todoItem.text}
+          />
+        ))}
+      </ul>
+    </>
   );
 }
