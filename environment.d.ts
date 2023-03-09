b9094571f922cea42a63c371d700b037afd93830
Conan Kunhwan Ahn <kunhwon@gmail.com>
2023-03-09 11:26:20 +0900

    환경변수 타입 추가

--- environment.d.ts
+++ environment.d.ts
@@ -1,6 +1,9 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
+    /** 서버에서만 사용하는 환경 변수 */
     readonly SERVER_VAR_1: string;
+
+    /** 서버와 클라이언트에서 모두 사용하는 환경 변수 */
     readonly NEXT_PUBLIC_VAR_1: string;
   }
 }
